fbc466276d0a7f37e7e93b1053aa5e73e93fc61c
성택 권 <kkst9602@naver.com>
2022-10-15 15:40:31 +0900

    코드 정리

--- Form1.cs
+++ Form1.cs
@@ -1,52 +1,34 @@
-using System.ComponentModel;
-using System.Runtime.InteropServices;
-using System.Threading;
-
 using System;
 using System.Diagnostics;
+using System.Runtime.InteropServices;
 
 namespace Capslock_to_KorEng
 {
 	public partial class Form1 : Form
 	{
 		private bool isPaused = false;
+
 		private GlobalKeyboardHook _globalKeyboardHook;
 
-		private const int VKLShift = 0xA0;
-		private const int VKCapslock = 0x14;
+		private const byte VKLShift = 0xA0;
+		private const byte VKCapslock = 0x14;
+		private const byte VKKanaMode = 0x15;
 
 		private const uint WM_KEYDOWN = 0x1;
 		private const uint WM_KEYUP = 0x2;
 
 		private bool isLShiftPressed = false;
 
-		// Hotkeys
-		[DllImport("user32.dll")]
-		public static extern bool RegisterHotKey(IntPtr hWnd, int id, int fsModifiers, int vlc);
-		[DllImport("user32.dll")]
-		public static extern bool UngegisterHotKey(IntPtr hWnd, int id);
-
 		// Send Key
 		[return: MarshalAs(UnmanagedType.Bool)]
 		[DllImport("user32.dll")]
 		static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, UIntPtr dwExtraInfo);
 
-		//private BackgroundWorker worker;
-
 		public Form1()
 		{
 			InitializeComponent();
-			//RegisterHotKey(this.Handle, myHotKeyID, 0, (int)Keys.CapsLock);
-			//worker = new BackgroundWorker();
 			SetupKeyboardHooks();
 		}
-
-		//private void worker_DoWork(object sender, DoWorkEventArgs e)
-		//{
-		//	BackgroundWorker bw = sender as BackgroundWorker;
-
-		//	int arg = (int)e.Argument;
-		//}
 
 		public void SetupKeyboardHooks()
 		{
@@ -59,9 +41,9 @@ namespace Capslock_to_KorEng
 		{
 			if (!isPaused)
 			{
-				Debug.WriteLine(e.KeyboardData.VirtualCode);
-				Debug.WriteLine(e.KeyboardData.Flags);
-				Debug.WriteLine(e.KeyboardState);
+				//Debug.WriteLine(e.KeyboardData.VirtualCode);
+				//Debug.WriteLine(e.KeyboardData.Flags);
+				//Debug.WriteLine(e.KeyboardState);
 
 				if (e.KeyboardData.VirtualCode != VKCapslock)
 					return;
@@ -73,12 +55,11 @@ namespace Capslock_to_KorEng
 				{
 					e.Handled = true;
 
-					keybd_event(byte.Parse(((int)Keys.KanaMode).ToString()), 0x45, WM_KEYDOWN, UIntPtr.Zero);
+					keybd_event(VKKanaMode, 0x45, WM_KEYDOWN, UIntPtr.Zero);
 					Thread.Sleep(50);
-					keybd_event(byte.Parse(((int)Keys.KanaMode).ToString()), 0x45, WM_KEYUP, UIntPtr.Zero);
+					keybd_event(VKKanaMode, 0x45, WM_KEYUP, UIntPtr.Zero);
 				}
 			}
-
 		}
 
 		private void SetLShiftStatus(object sender, GlobalKeyboardHookEventArgs e)
